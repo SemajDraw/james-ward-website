c07ebf3d777dd757ed88f1eecc605cf16a1ffe1e
SemajDraw <32390574+SemajDraw@users.noreply.github.com>
2019-09-02 08:24:12 +0100

    Home page finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PassionComponent } from './home/passion/passion.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InformationComponent } from './home/information/information.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    PassionComponent,
+    NavbarComponent,
+    InformationComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
